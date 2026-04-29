606a325345b8c6166ae7063b0332eaecd365bc1f
qiu <zhengshen@nextbillion.ai>
2026-04-29 17:57:37 +0800

    chore : bump to 3.3.0

--- Package.swift
+++ Package.swift
@@ -13,23 +13,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NbmapNavigation",
-            url: "https://github.com/nextbillion-ai/nextbillion-navigation-ios/releases/download/3.2.0/NbmapNavigation.xcframework.zip",
+            url: "https://github.com/nextbillion-ai/nextbillion-navigation-ios/releases/download/3.3.0/NbmapNavigation.xcframework.zip",
             checksum: "093a82c4281cd3fb9d7b14fe0eb1aafa99b013a38d671a9253ccf7143928ac3e"
         ),
         .binaryTarget(
             name: "NbmapCoreNavigation",
-            url: "https://github.com/nextbillion-ai/nextbillion-navigation-ios/releases/download/3.2.0/NbmapCoreNavigation.xcframework.zip",
-            checksum: "43251e2cdc07eec6d46ff391dc255a8b0140fba80330cfa9ed3d5bca9f5f9b15"
+            url: "https://github.com/nextbillion-ai/nextbillion-navigation-ios/releases/download/3.3.0/NbmapCoreNavigation.xcframework.zip",
+            checksum: "43cac0a3191cbdb46ea2ae2d87f1479233f27a24ee3e9283c4b04e116cb0f508"
         ),
         .binaryTarget(
             name: "Nbmap",
-            url: "https://github.com/nextbillion-ai/nextbillion-map-ios/releases/download/2.1.2/Nbmap.xcframework.zip",
-            checksum: "0f48b5cd6af5157296ecea043eb08515f42d524f701e80e02a1fe29ec2089636"
+            url: "https://github.com/nextbillion-ai/nextbillion-map-ios/releases/download/2.1.5/Nbmap.xcframework.zip",
+            checksum: "e19a1e0a3a92344fca69bb319f8eecae79c4e2d94b36f9993d791e06480fd7d2"
         ),
         .binaryTarget(
             name: "Turf",
             url: "https://github.com/nextbillion-ai/nextbillion-turf-ios/releases/download/3.0.1/Turf.xcframework.zip",
-            checksum: "086e1ca86c2ba5e3e5b8bdd6811d5bebd26653aa2033bc98da0a7d21dcc3c7a3"
+            checksum: "1302f517c75d356137430a15be7890686a23ac247501f46d03ba9fe972f56623"
         )
     ]
 )
